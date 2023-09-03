4dae19b858d2d1a496f008aae601c67d98d07d9d
tryfan <tryfan.duncton@gmail.com>
2023-09-03 09:14:10 -0400

    add clipboard stuff

--- nvim/.config/nvim/lua/ncelebic/remap.lua
+++ nvim/.config/nvim/lua/ncelebic/remap.lua
@@ -1,3 +1,5 @@
+vim.api.nvim_set_option("clipboard","unnamedplus")
+
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Directory Explorer" })
 
@@ -5,4 +7,6 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("v", "<leader>y", '"+y')
 
